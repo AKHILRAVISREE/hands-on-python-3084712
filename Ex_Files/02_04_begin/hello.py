429b960c3005b4036d31cbc423b01373afc422b6
AKHILRAVISREE <47850796+AKHILRAVISREE@users.noreply.github.com>
2025-01-05 17:55:38 +0000

    change

--- Ex_Files/02_04_begin/hello.py
+++ Ex_Files/02_04_begin/hello.py
@@ -2,12 +2,18 @@ Name = ["Akhil", "Ravi", "Sreeja", "Soubhagya"]
 Ages = [10,20,30,40]
 
 i = 0
-while i < len(Name):
+#while i < len(Name):
   #print(Name[i])
-  i=i+1
+ # i=i+1
 
 #for name in Name:
   #print(name)
 
-for name, age in zip(Name,Ages):
-  print(f"Name:{name},Age:{age}")
+#for name, age in zip(Name,Ages):
+  #print(f"Name:{name},Age:{age}")
+
+#for i in range(5):
+  #print(i)
+
+for i, name in enumerate(Name):
+  print(f"{i},{name}")
