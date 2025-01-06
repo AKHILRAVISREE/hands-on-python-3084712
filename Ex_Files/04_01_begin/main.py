26c73adaa5b81c6aa4398271cb99aa5f66499fc4
AKHILRAVISREE <47850796+AKHILRAVISREE@users.noreply.github.com>
2025-01-06 17:12:05 +0000

    my change

--- Ex_Files/04_01_begin/main.py
+++ Ex_Files/04_01_begin/main.py
@@ -1,8 +1,13 @@
 import requests
+import json
 
 response = requests.get(
     "http://api.worldbank.org/v2/countries/USA/indicators/SP.POP.TOTL?per_page=5000&format=json",
 )
 
 last_twenty_years = response.json()[1][:20]
+
+for year in last_twenty_years:
+  val = year["value"]
+  print(f'{year["date"]}: {year["value"]}', "="*val)
 
